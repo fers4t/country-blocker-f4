00cb15ceddc8d2921a96667867f0a6ac97efc06b
Ferşat <58539842+fers4t@users.noreply.github.com>
2021-03-01 05:25:41 +0300

    style

--- view/panel.php
+++ view/panel.php
@@ -2,20 +2,22 @@
 $root_path = dirname(ABSPATH);
 
 ?>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=custompage" method="post">
-    <div style="display: block; margin-bottom: 15px">
-        <label for="country_code"><?php echo __('Please enter your country code', 'textdomain') ?></label>
-        <input type="text" name="country_code" id="country_code">
-        <small><?php echo __('All countries except this will be blocked', 'textdomain'); ?></small>
-    </div>
-    <div style="display: block; margin-bottom: 15px">
-        <label for="panel_url">
-            <?php echo __('Please enter your admin panel slug.', 'textdomain') ?>
-        </label>
-        <input type="text" name="panel_url" placeholder="Ex: wp-admin">
-    </div>
-    <input type="submit" value="Block All" name="country_submit">
-</form>
+<div id="f4_country_blocker">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=custompage" method="post">
+        <div>
+            <label for="country_code"><?php echo __('Please enter your country code', 'textdomain') ?></label>
+            <input type="text" name="country_code" id="country_code">
+            <small><?php echo __('All countries except this will be blocked', 'textdomain'); ?></small>
+        </div>
+        <div>
+            <label for="panel_url">
+                <?php echo __('Please enter your admin panel slug.', 'textdomain') ?>
+            </label>
+            <input type="text" name="panel_url" placeholder="Ex: wp-admin">
+        </div>
+        <input type="submit" value="Block All" name="country_submit">
+    </form>
+</div>
 <?php
 
 require_once(CB_PLUGIN_PATH . "/controller/form_action.php");
@@ -26,15 +28,17 @@ $allowed_country = file_get_contents($root_path . "/blocked_countries/countries.
 if (!empty($allowed_country)) {
     $country_code = $allowed_country['code'];
     $panel_url = $allowed_country['url']; ?>
-    <div style="display: block">
-        <h3 style="display:inline-block">
-            <?php echo __('Your admin panel can only visit by this country:', 'textdomain') ?>
-        </h3> <?php echo $country_code; ?>
-    </div>
-    <div style="display: block">
-        <h3 style="display:inline-block">
-            <?php echo __('Your panel slug:', 'textdomain') ?>
-        </h3> <?php echo $panel_url; ?>
+    <div id="f4_informations">
+        <div style="display: block">
+            <h3 style="display:inline-block">
+                <?php echo __('Your admin panel can only visit by this country:', 'textdomain') ?>
+            </h3> <?php echo $country_code; ?>
+        </div>
+        <div style="display: block">
+            <h3 style="display:inline-block">
+                <?php echo __('Your panel slug:', 'textdomain') ?>
+            </h3> <?php echo $panel_url; ?>
+        </div>
     </div>
     <?php
 }
