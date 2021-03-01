231cf19ce07e1ed7d38ab2d340980204efacb484
Ferşat <58539842+fers4t@users.noreply.github.com>
2021-03-01 05:23:22 +0300

    translatable strings

--- view/panel.php
+++ view/panel.php
@@ -4,14 +4,17 @@ $root_path = dirname(ABSPATH);
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=custompage" method="post">
     <div style="display: block; margin-bottom: 15px">
-        <label for="country_code">Lütfen ülke kodunu girin</label>
+        <label for="country_code"><?php echo __('Please enter your country code', 'textdomain') ?></label>
         <input type="text" name="country_code" id="country_code">
+        <small><?php echo __('All countries except this will be blocked', 'textdomain'); ?></small>
     </div>
     <div style="display: block; margin-bottom: 15px">
-        <label for="panel_url">Lütfen panel urlnizi giriniz</label>
-        <input type="text" name="panel_url" placeholder="Örn: wp-admin">
+        <label for="panel_url">
+            <?php echo __('Please enter your admin panel slug.', 'textdomain') ?>
+        </label>
+        <input type="text" name="panel_url" placeholder="Ex: wp-admin">
     </div>
-    <input type="submit" value="Yasakla" name="country_submit">
+    <input type="submit" value="Block All" name="country_submit">
 </form>
 <?php
 
@@ -24,10 +27,14 @@ if (!empty($allowed_country)) {
     $country_code = $allowed_country['code'];
     $panel_url = $allowed_country['url']; ?>
     <div style="display: block">
-        <h3 style="display:inline-block">Sitenizin admin paneli yalnızca şu ülke için açıktır:</h3> <?php echo $country_code; ?>
+        <h3 style="display:inline-block">
+            <?php echo __('Your admin panel can only visit by this country:', 'textdomain') ?>
+        </h3> <?php echo $country_code; ?>
     </div>
     <div style="display: block">
-        <h3 style="display:inline-block">Panel Urlniz:</h3> <?php echo $panel_url; ?>
+        <h3 style="display:inline-block">
+            <?php echo __('Your panel slug:', 'textdomain') ?>
+        </h3> <?php echo $panel_url; ?>
     </div>
     <?php
 }
